e2ead549b021db57d78f7fbd17f7706924f19e27
kuru-shu <kurushu71@gmail.com>
2024-09-24 01:04:50 +0900

    Turn off echoing

--- kilo.c
+++ kilo.c
@@ -1,8 +1,14 @@
+#include <termios.h>
 #include <unistd.h>
+void enableRawMode() {
+  struct termios raw;
+  tcgetattr(STDIN_FILENO, &raw);
+  raw.c_lflag &= ~(ECHO);
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
 int main() {
+  enableRawMode();
   char c;
-  // 標準入力のバイト数の読み込み
-  // 1バイトであれば続ける
   while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
   return 0;
 }
