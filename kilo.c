6f8b711821dcd72595c47b33e86f13662f9f4e12
kuru-shu <kurushu71@gmail.com>
2024-09-24 01:08:39 +0900

    add comment

--- kilo.c
+++ kilo.c
@@ -2,8 +2,11 @@
 #include <unistd.h>
 void enableRawMode() {
   struct termios raw;
+  // 現在の画面環境を取得
   tcgetattr(STDIN_FILENO, &raw);
+  // 現在の環境でechoをオフにする
   raw.c_lflag &= ~(ECHO);
+  // 現在の画面環境を更新
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 int main() {
