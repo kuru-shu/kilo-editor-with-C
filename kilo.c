6057a786c76b1551c21034470b77d95a05eccaf2
kuru-shu <kurushu71@gmail.com>
2024-09-25 00:49:50 +0900

    Disable raw mode at exit

--- kilo.c
+++ kilo.c
@@ -1,9 +1,20 @@
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
+
+struct termios orig_termios;
+void disableRawMode() {
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
 void enableRawMode() {
-  struct termios raw;
+
   // 現在の画面環境を取得
-  tcgetattr(STDIN_FILENO, &raw);
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  // プログラム終了時に呼び出す
+  atexit(disableRawMode);
+
+  struct termios raw = orig_termios;
   // 現在の環境でechoをオフにする
   raw.c_lflag &= ~(ECHO);
   // 現在の画面環境を更新
